0b7fa048467a41a2a59b72dae084e6527b4a2b1a
trustpizza <maxelolsson@gmail.com>
2022-12-21 21:55:45 -0600

    Likes can now be generated

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @tweet = Tweet.find(like_params[:tweet_id])
+    @like = @tweet.likes.create(like_params)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :tweet_id)
+  end
 end
