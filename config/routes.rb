ccb5902a42e0e891984f02b19e323286d26d91ac
trustpizza <maxelolsson@gmail.com>
2022-12-21 19:35:51 -0600

    Add views I anticipate needing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :tweets
+  end
 
+  root 'tweets#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_22_012254) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_013059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_012254) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
     t.index ["author_id"], name: "index_tweets_on_author_id"
   end
 
