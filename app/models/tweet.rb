6c7cc50c9eefc354dfebf2ff8e19d769599c8b7d
trustpizza <maxelolsson@gmail.com>
2022-12-22 10:24:35 -0600

    Create liked_by? and find_like methods

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,6 +10,10 @@ class Tweet < ApplicationRecord
 
   scope :ordered, -> {order(id: :desc)}
 
+  def liked_by?(user)
+  likes.any? { |like| like.user == user }
+  end
+
   def find_like(user)
     likes.find_by(user: user)
   end
