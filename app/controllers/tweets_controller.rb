70839adb43ce5178c9673499297522b8d040396b
trustpizza <maxelolsson@gmail.com>
2022-12-21 19:29:47 -0600

    Add skeleton of Tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,22 @@
 class TweetsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
 end
