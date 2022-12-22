e538cdc0f8534ff727838ae1c12695b1ec2fefb2
trustpizza <maxelolsson@gmail.com>
2022-12-21 22:14:33 -0600

    Fix routes, users can now like posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users do
-    resources :tweets, only: [:new, :delete, :create ,:show]
-    resources :likes, only: [:new, :delete, :create]
+    resources :tweets, only: [:new, :index, :create]
   end
 
-  resources :tweets, only: [:index]
+
+  resources :tweets, only: [:show, :edit, :update, :destroy] do
+    resources :likes
+
+  end
 
   root 'tweets#index'
   # Defines the root path route ("/")
