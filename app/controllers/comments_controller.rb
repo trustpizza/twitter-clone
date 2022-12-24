598bebdb7ee3c26e5e6726da249e8de18843488a
trustpizza <maxelolsson@gmail.com>
2022-12-24 16:06:44 -0600

    Create skeleton of comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def comment_params
+  end
+end
