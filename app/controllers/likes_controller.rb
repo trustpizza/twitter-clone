486a2e79c356b4fbacdf5984b4fc6c8910304b9a
trustpizza <maxelolsson@gmail.com>
2022-12-24 15:56:27 -0600

    Make the delete button also a turbo-stream

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -19,7 +19,14 @@ class LikesController < ApplicationController
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     @like = @tweet.likes.find_by(like_params)
-    @like.destroy 
+
+    respond_to do |format|
+      if @like.destroy 
+        format.turbo_stream
+      else
+        format.html {render :new}
+      end
+    end
   end
 
   private
