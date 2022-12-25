c935c53917beebc33d7da2ade45ad8738d9ec812
trustpizza <maxelolsson@gmail.com>
2022-12-25 15:58:40 -0600

    Order posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :tweet
   belongs_to :commenter, class_name: "User"
 
+  scope :ordered, -> {order(id: :desc)}
+
   validates :body, presence: true, length: { in: 1..140 }
 end
