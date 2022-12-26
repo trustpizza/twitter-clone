b315f0cbce2d0936b2ac1d46ee210b4263be2c66
trustpizza <maxelolsson@gmail.com>
2022-12-26 10:23:32 -0600

    Create profile model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update
-    if @user.update_attributes(user_params)
-      flash[:success] = 'Updated!'
-      redirect_to @user
-    else  
-      render 'edit'
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:bio)
-  end
 end
