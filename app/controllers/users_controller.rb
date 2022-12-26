82e0080d4a281543bb84721c6b5d35b746f09488
trustpizza <maxelolsson@gmail.com>
2022-12-26 09:14:00 -0600

    Have everything but the nested bio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Updated!'
+      redirect_to @user
+    else  
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:bio)
+  end
 end
