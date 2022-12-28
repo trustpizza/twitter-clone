654cf39a3a89bb60430492c774f49187c18dff7e
trustpizza <maxelolsson@gmail.com>
2022-12-28 16:00:22 -0600

    Still not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,13 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
 
-  scope :popular, -> { joins(:followers).group('sender_id').order(:count) }
+  # Scopes
+
+  scope :popular, -> { joins(:followers).group('sender_id').order(count('id')) }
+
+  def followers_count
+    self.followers.count
+  end
   # Followers have a receiver id that matches up to a Users Id
   # Order by the number of receiver id's that match to a Users Id
   # Show the user with the most matches
