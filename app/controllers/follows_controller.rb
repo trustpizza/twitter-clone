250dc582f17236d2c9db8d8eafc74db04b4f5319
trustpizza <maxelolsson@gmail.com>
2022-12-29 18:33:04 -0600

    Make #new and #create methods

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -6,11 +6,25 @@ class FollowsController < ApplicationController
   end
   
   def new
+    @follow = Follow.new
   end
 
   def create
+    @follow = Follow.create(follow_params)
+
+    if @follow.save
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def follow_params
+    params.require(:follow).permit(:sender_id, :receiver_id)
   end
 end
