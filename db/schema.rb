cccb7d116b505d9d8e8aae7d39d8f206f67a3aa0
trustpizza <maxelolsson@gmail.com>
2023-01-02 14:34:53 -0600

    Fixed nil values in follows_count by adding default values

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_184223) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "follows_count"
+    t.integer "follows_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
