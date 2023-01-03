99131fd969371a7b57a9f3ee8c0b10fa590cf517
trustpizza <maxelolsson@gmail.com>
2023-01-03 14:06:08 -0600

    I think gravatar is working?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  include Gravtastic
+  gravtastic 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 
   has_many :tweets, foreign_key: "author_id", class_name: "Tweet", dependent: :destroy
   has_many :likes, dependent: :destroy
