c0bb3be13db7f648858a91d6736c211484e9b2ef
trustpizza <maxelolsson@gmail.com>
2023-01-05 19:16:21 -0600

    Permitted new params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    added_params = [:name]
+
+    devise_parameter_sanitizer.permit :sign_up, keys: added_params
+    devise_parameter_sanitizer.permit :account_update, keys:  added_params
+  end
 end
