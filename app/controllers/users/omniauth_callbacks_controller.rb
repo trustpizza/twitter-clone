a21729a3b0ec1d4efbfa49efebd1a77e975e74ef
trustpizza <maxelolsson@gmail.com>
2023-01-06 14:55:26 -0600

    Awaiting feedback on omniauth facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :facebook
+ 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,7 +274,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, "853213062565726", "89aa7e8105419cc3e8436d26c4993c21", token_params: { parse: :json }
-
+  #config.omniauth :facebook, "832034401480525", "56bfa2878fccd42823f0688e549661d0"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
